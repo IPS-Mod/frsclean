a4bc47237a125f65bc6688152734cfe26d09c28e
Damon Morris <d.j.morris@sheffield.ac.uk>
2023-06-16 15:11:20 +0100

    tidy up compulsory expenditure variables

--- R/Clean_Expenditure.R
+++ R/Clean_Expenditure.R
@@ -26,52 +26,52 @@ clean_expenditure <- function(data,
   #### Create clean expenditure variables ####
 
   ## Maintenance payments
-  maint_data[, x_mp := 0]
-  maint_data[, x_mp := mramt*(52/12)]
-  maint_data[mrus == 2, x_mp := mruamt*(52/12)]
+  maint_data[, xmp := 0]
+  maint_data[, xmp := mramt*(52/12)]
+  maint_data[mrus == 2, xmp := mruamt*(52/12)]
 
   clean_data <- merge(clean_data, maint_data, by = c("sernum", "benunit", "person"), all.x = TRUE)
 
-  clean_data[is.na(x_mp), x_mp := 0]
+  clean_data[is.na(xmp), xmp := 0]
 
   ## Housing costs (rent) (household responsible person only)
-  clean_data[, x_hcost_rent := 0]
-  clean_data[!is.na(hhrent) & hhrent > 0, x_hcost_rent := hhrent*(52/12)]
-  clean_data[hrpid != 1, x_hcost_rent := 0]
+  clean_data[, xhcrt := 0]
+  clean_data[!is.na(hhrent) & hhrent > 0, xhcrt := hhrent*(52/12)]
+  clean_data[hrpid != 1, xhcrt := 0]
 
   ## Mortgage interest
-  clean_data[, x_hcost_mint := 0]
-  clean_data[!is.na(mortint) & mortint > 0, x_hcost_mint := mortint*(52/12)]
-  clean_data[hrpid != 1, x_hcost_mint := 0]
+  clean_data[, xhcmomi := 0]
+  clean_data[!is.na(mortint) & mortint > 0, xhcmomi := mortint*(52/12)]
+  clean_data[hrpid != 1, xhcmomi := 0]
 
   ## Housing costs (other) - water and sewage costs
-  clean_data[, x_hcost_oth := 0]
-  clean_data[gvtregno == 12 & cwatamtd >= 0, x_hcost_oth := x_hcost_oth + cwatamtd] ## Scot
-  clean_data[gvtregno == 12 & csewamt >= 0, x_hcost_oth := x_hcost_oth + csewamt]     ## Scot
-  clean_data[gvtregno < 12 & watsewrt >= 0, x_hcost_oth := x_hcost_oth + watsewrt]  ## Eng/Wales
-  clean_data[, x_hcost_oth := x_hcost_oth*(52/12)]
-  clean_data[hrpid != 1, x_hcost_oth := 0]
+  clean_data[, xhcot := 0]
+  clean_data[gvtregno == 12 & cwatamtd >= 0, xhcot := xhcot + cwatamtd] ## Scot
+  clean_data[gvtregno == 12 & csewamt >= 0, xhcot := xhcot + csewamt]     ## Scot
+  clean_data[gvtregno < 12 & watsewrt >= 0, xhcot := xhcot + watsewrt]  ## Eng/Wales
+  clean_data[, xhcot := xhcot*(52/12)]
+  clean_data[hrpid != 1, xhcot := 0]
 
   ## Total housing costs
-  clean_data[, x_hcost := 0]
-  clean_data[gvtregno != 13 & gbhscost != -1, x_hcost := gbhscost]
-  clean_data[gvtregno != 13 & nihscost != -1, x_hcost := nihscost]
-  clean_data[, x_hcost := x_hcost*(52/12)]
-  clean_data[hrpid != 1, x_hcost := 0]
+  clean_data[, xhc := 0]
+  clean_data[gvtregno != 13 & gbhscost != -1, xhc := gbhscost] ## GB
+  clean_data[gvtregno == 13 & nihscost != -1, xhc := nihscost] ## Northern Ireland
+  clean_data[, xhc := xhc*(52/12)]
+  clean_data[hrpid != 1, xhc := 0]
 
   ## Private pension provision
-  penprov_data[, x_ppen := 0]
-  penprov_data[stemppen %in% 5:6, x_ppen := penamt*(52/12)]
-  penprov_data <- penprov_data[, .(x_ppen = sum(x_ppen)), by = c("sernum", "person", "benunit")]
+  penprov_data[, xpp := 0]
+  penprov_data[stemppen %in% 5:6, xpp := penamt*(52/12)]
+  penprov_data <- penprov_data[, .(xpp = sum(xpp)), by = c("sernum", "person", "benunit")]
 
   clean_data <- merge(clean_data, penprov_data, by = c("sernum", "benunit", "person"), all.x = TRUE)
-  clean_data[is.na(x_ppen), x_ppen := 0]
+  clean_data[is.na(xpp), xpp := 0]
 
   ############################
   #### Retain variables ######
 
   exp_vars <- Hmisc::Cs(sernum, benunit, person,
-                        x_mp, x_hcost, x_hcost_rent, x_hcost_mint, x_ppen, x_hcost_oth)
+                        xmp, xhc, xhcrt, xhcmomi, xhcot, xpp)
 
 
   clean_data <- clean_data[ , exp_vars, with = F]
